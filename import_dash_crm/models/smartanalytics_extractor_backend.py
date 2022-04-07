ce5b58a669654bce60d5edcb01aea21dddbe379d
Idealis Consulting <2088903+idealisconsultingbe@users.noreply.github.com>
2022-04-07 16:54:40 +0200

    Add files via upload

---
+++ import_dash_crm/models/smartanalytics_extractor_backend.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class SmartanalyticsExtractorBackendCrm(models.Model):
+    _inherit = 'smartanalytics.extractor.backend'
---
+++ import_dash_hr/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "import_dash_hr",
+
+    'author': "Idealis Consulting",
+    'website': "http://www.idealisconsulting.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'hr', 'dashboard_frame'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'data/smart.analytics.dashboard.csv',
+        'data/smartanalytics.extractor.backend.csv',
+        'data/smartanalytics.extractor.extract.csv',
+    ],
+    'auto-install': False,
+
+}
